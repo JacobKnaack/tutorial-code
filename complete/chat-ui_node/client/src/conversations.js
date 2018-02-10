beed3736767a70e2c247a09f06e20737530bb494
Jacob Knaack <jacobknck@gmail.com>
2018-02-10 11:42:21 -0800

    added http requests for fetching users and refactored data in json mock database

---
+++ complete/chat-ui_node/client/src/conversations.js
@@ -0,0 +1,49 @@
+function getUsers(callback) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200)
+      callback(request.response);
+  }
+  request.open("GET", 'http://localhost:3000/mockUsers', true);
+  request.send(null);
+};
+
+function toggleConversationForm() {
+  var modalForm = document.querySelector(".form.hidden");
+  var formEl = document.getElementById("conversationFormContainer");
+
+  if(modalForm) {
+    modalForm.classList.remove("hidden");
+  } else {
+    formEl.classList.add("hidden");
+    document.getElementById("conversationForm").reset();
+    document.getElementById('searchResults').innerHTML = "";
+  }
+}
+
+function addConversation() {
+
+}
+
+window.addEventListener("DOMContentLoaded", function(){
+  getUsers(function(Users) {
+    let UserObject = JSON.parse(Users);
+
+    document.getElementById("searchField").addEventListener("input", function (e) {
+      document.getElementById("searchResults").innerHTML = "";
+      for ( var item in UserObject ) {
+        if ( UserObject[item].firstName.toLowerCase().includes(this.value.toLowerCase()) ) {
+          document.getElementById("searchResults").innerHTML += "\
+            <p class='usersResult' onclick='addUser(\"" + UserObject[item].id +"\")'>"
+               + UserObject[item].firstName +
+            "</p>"
+        }
+        if ( document.getElementById("searchField").value.length == 0) {
+          document.getElementById("searchResults").innerHTML = "";
+        } 
+      }
+    });
+
+  });
+
+});
--- complete/chat-ui_node/server.js
+++ complete/chat-ui_node/server.js
@@ -1,14 +1,53 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const http = require('http').Server(app);
 const path = require('path');
-
+const bodyParser = require('body-parser');
 //initialize the socket.io instance and pass in the http server object
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI || 'mongodb://admin:admin@ds229918.mlab.com:29918/purple-chat'
+const mockData = require('./client/src/db.json');
 
+var Conversations = mongoose.model('Conversation', {
+  name: String,
+  chat: String
+});
+
+mongoose.connect(mongoUri, (err) => {
+  console.log('Database Connection ::: Error:', err);
+});
+
+//serving client src code
 app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// fetching mock data
+app.get('/mockUsers', (req, res) => {
+  try {
+    res.set('Content-Type', 'application/json');
+    res.send(mockData.users);
+  } catch (error) {
+    res.sendStatus(500)
+    res.send(error);
+    console.error('Server error: ', error);
+  }
+});
+
+app.post('/chat', async (req, res) => {
+  try {
+    var chat = new Conversation(req.body);
+    await chat.save();
+    res.sendStatus(200);
+    res.send(chat);
+  } catch (error) {
+    res.sendStatus(500);
+    console.error(error);
+  }
+});
 
 //listen for a connection
 io.on('connection', function(socket){
