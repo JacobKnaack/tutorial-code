82f6462c4ad9206631a645a3ed872be9c4702f8a
Jacob Knaack <jacobknck@gmail.com>
2018-02-20 10:09:02 -0800

    removed node modules

--- complete/chat-ui_node/client/src/conversations.js
+++ complete/chat-ui_node/client/src/conversations.js
@@ -21,12 +21,34 @@ function toggleConversationForm() {
   }
 }
 
-function addConversation() {
-  // function for adding conversation elements to the conversation containers
+function addConversation(id) {
+  var users = JSON.parse(localStorage.getItem('PurpleChatUsers'));
+  var menuEls = "";
+
+  for (var user in users) {
+    if (users[user].id === parseInt(id)) {
+      menuEls += "<div id="+ users[user].id +" class=chat-item onclick='selectChat(\"" + users[user].id +"\")'><i class='fa fa-user' aria-hidden='true'></i> " + users[user].userName + "<div id='contactInfo-small'><i class='fa fa-phone' aria-hidden='true'></i><i class='fa fa-envelope' aria-hidden='true'></i><i class='fa fa-map-marker' aria-hidden='true'></i></div></div>";
+      selectChat(users[user].id);
+    }
+    document.getElementById("conversation-data").innerHTML = menuEls;
+  }
+  toggleConversationForm();
+}
+
+function selectChat(id) {
+  let users = JSON.parse(localStorage.getItem('PurpleChatUsers'));
+
+  for (var user in users) {
+    if (users[user].id === parseInt(id)) {
+      localStorage.setItem('SelectedPurpleChat', JSON.stringify(users[user]));
+      console.log('chat selected: ', users[user]);
+    }
+  }
 }
 
 window.addEventListener("DOMContentLoaded", function(){
   getUsers(function(Users) {
+    localStorage.setItem("PurpleChatUsers", Users)
     let UserObject = JSON.parse(Users);
 
     document.getElementById("searchField").addEventListener("input", function (e) {
@@ -34,7 +56,7 @@ window.addEventListener("DOMContentLoaded", function(){
       for ( var item in UserObject ) {
         if ( UserObject[item].userName.toLowerCase().includes(this.value.toLowerCase()) ) {
           document.getElementById("searchResults").innerHTML += "\
-            <p class='usersResult' onclick='addUser(\"" + UserObject[item].id +"\")'>"
+            <p class='usersResult' onclick='addConversation(\"" + UserObject[item].id +"\")'>"
                + UserObject[item].userName +
             "</p>"
         }
